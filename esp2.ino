6f62c33da80bec1edaef61d498e3b7ad6e5fb3d7
NatroN <natron2e@gmail.com>
2016-02-09 00:15:01 +0100

    update

--- esp2.ino
+++ esp2.ino
@@ -5,11 +5,12 @@
 #include <Arduino.h>
 #include <WebSocketsServer.h>
 #include <Hash.h>
+#include "RTClib.h"
+#include <Wire.h>
 
 
 
-
-#define BLUEPIN 12
+#define BLUEPIN 13
 
 #define SSID_ME "Wlan07"
 #define PW_ME "wlan_01!_2005!grojer_.."
@@ -39,6 +40,12 @@ int zOld;
  Read char value from Serial Monitor
  Display that value on the Serial Monitor
  */
+String testString = "ESP";
+int testInt = 1;
+float testFloat = 2;
+byte testByte = 3;
+boolean testBoolean = false;
+DateTime testDateTime;
 
 String StringFromSerial;
 unsigned long sent = millis();
@@ -53,6 +60,7 @@ void setup()
   Serial.begin(115200);     
   Serial.println("Serial Monitor Connected");  
   pinMode(BLUEPIN, OUTPUT);
+  analogWriteFreq(200);
   WifiConnect();
   WebSocketConnect();
   HTTPUpdateConnect();
@@ -98,16 +106,111 @@ void loop()
       }   
 }
 
+
 void parseCommand(String com)
-{ String part1;
-part1 = com.substring(0, com.indexOf("|"));
-  if(part1.equalsIgnoreCase("ack"))
-  {i=0;
-   //sendCommand("GiveMillis");
-  // sendCommand("How long will  ESP8266 to an Arno Mega via Serial line?");
-   //sendCommand("GiveMillis");
-  }  
+{ 
+String part1;
+ // part1 = com.substring(0, com.indexOf("|"));
+  if(com.substring(0, com.indexOf("|")).equalsIgnoreCase("DataForYou"))
+    {
+    for(int i=com.indexOf("|"); i<com.lastIndexOf("|"); i=com.indexOf("|",i+1))
+     {String part1 = com.substring(i+1, com.indexOf("|",i+1));
+      //____________________________________________________________________________________________________use this to parseStrings
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("teststring"))
+      {
+        testString=part1.substring(part1.indexOf("_")+1,part1.length());
+        
+      }
+      //____________________________________________________________________________________________________use this to parseInts
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("testInt"))
+      {
+        testInt=(part1.substring(part1.indexOf("_")+1,part1.length())).toInt();
+        
+      }      
+      //____________________________________________________________________________________________________use this to parseFloats
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("testFloat"))
+      {
+        testFloat=(part1.substring(part1.indexOf("_")+1,part1.length())).toFloat();
+        
+      }    
+       //____________________________________________________________________________________________________use this to parseByte
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("testByte"))
+      {
+        testByte=(part1.substring(part1.indexOf("_")+1,part1.length())).toInt();
+        
+      }  
+       //____________________________________________________________________________________________________use this to parseBoolean
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("testBoolean"))
+      {
+        testBoolean=(part1.substring(part1.indexOf("_")+1,part1.length())).toInt();
+        
+      }  
+       //____________________________________________________________________________________________________use this to parseByte
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("testBoolean"))
+      {
+        testBoolean=(part1.substring(part1.indexOf("_")+1,part1.length())).toInt();
+        
+      }  
+       //____________________________________________________________________________________________________use this to parsDateTime in Seconds unixtimne!
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("testDateTime"))
+      {
+        char arr[12];
+        (part1.substring(part1.indexOf("_")+1,part1.length())).toCharArray(arr, sizeof(arr));
+       testDateTime=atol(arr);
+        
+      } 
+      else
+      {
+        sendCommand(String(com));
+      }
+       
+    }
+    }
+  else  if(com.substring(0, com.indexOf("|")).equalsIgnoreCase("DataForMe"))
+    {
+    for(int i=com.indexOf("|"); i<com.lastIndexOf("|"); i=com.indexOf("|",i+1))
+     {String part1 = com.substring(i+1, com.indexOf("|",i+1));
+      //____________________________________________________________________________________________________use this to parseStrings
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("teststring"))
+      {
+        sendCommand(testString);
+      }
+      //____________________________________________________________________________________________________use this to parseInts
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("testInt"))
+      {
+        sendCommand(String(testInt));
+      }      
+      //____________________________________________________________________________________________________use this to parseFloats
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("testFloat"))
+      {
+        sendCommand(String(testFloat));
+      }    
+       //____________________________________________________________________________________________________use this to parseByte
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("testByte"))
+      {
+        sendCommand(String(testByte));
+      }  
+       //____________________________________________________________________________________________________use this to parseBoolean
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("testBoolean"))
+      {
+        sendCommand(String(testBoolean));
+      }  
+       //____________________________________________________________________________________________________use this to parsDateTime in Seconds unixtimne!
+      if(part1.substring(0, part1.indexOf("_")).equalsIgnoreCase("testDateTime"))
+      {        sendCommand(String(testDateTime.unixtime()));
+      }  
+    } 
+    }
+
+  else
+   {
+    //Serial.println(com);
+   }
+  
 }
+
+
+
 
 
 
@@ -117,7 +220,7 @@ void sendCommand(String sendCom)
   String sendThis = sendCom + "| \n" ;
    char charBuf[100];
    sendThis.toCharArray(charBuf, 100);
-   Serial.write(charBuf);
+   Serial.println(charBuf);
 }
 
 
@@ -153,6 +256,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
           int zInt = zVal.toInt();
           analogWrite(BLUEPIN, zInt);
           webSocket.broadcastTXT(text);
+          
         }
 
         else if (text.startsWith("z")) {
@@ -161,9 +265,31 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
           Serial.println(zInt);
           analogWrite(BLUEPIN, zInt);
         }
+          else if (text.startsWith("y")) {
+          String zVal = (text.substring(text.indexOf("y") + 1, text.length()));
+          //analogWrite(BLUEPIN, zInt);
+          webSocket.broadcastTXT(testString);
+          webSocket.broadcastTXT(String(testInt));
+          webSocket.broadcastTXT(String(testFloat));
+          webSocket.broadcastTXT(String(testByte));
+          webSocket.broadcastTXT(String(testBoolean));
+
+        webSocket.broadcastTXT(String(testDateTime.hour(), DEC));
+        webSocket.broadcastTXT(String(':'));
+        webSocket.broadcastTXT(String(testDateTime.minute(), DEC));
+        webSocket.broadcastTXT(String(':'));
+        webSocket.broadcastTXT(String(testDateTime.second(), DEC));
+        webSocket.broadcastTXT(String(' '));
+        webSocket.broadcastTXT(String(testDateTime.day(), DEC));
+        webSocket.broadcastTXT(String('.'));
+        webSocket.broadcastTXT(String(testDateTime.month(), DEC));
+        webSocket.broadcastTXT(String('.'));
+        webSocket.broadcastTXT(String(testDateTime.year(), DEC));
+          
+        }
       else
       {
-        //sendCommand(text);
+        sendCommand(text);
       }
 
         
